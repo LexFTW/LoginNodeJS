73cd22c8e84128b64b3878c0bc0bb9b5dcc3379a
LexFTW <lexmengual@gmail.com>
2019-02-07 19:08:48 +0100

    Login Working

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+const parser = require('body-parser');
+const app = express();
+const path = require('path');
+const engine = require('ejs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const users = [
+  {name : 'amengual', password : '1234'},
+  {name : 'duson', password : '1234'},
+  {name : 'emieza', password : '1234'}
+]
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.use(parser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+
+// routes
+app.get('/', function(req, res){
+  res.render('index');
+})
+
+app.post('/', function(req, res){
+  console.log('[INFO] - Checking name, wait...')
+  for (var i = 0; i < users.length; i++) {
+    if(users[i]['name'] == req.body.name && users[i]['password'] == req.body.password){
+      console.log("[INFO] - Correct User introduce.");
+      res.send('[INFO]. Name: ' + req.body.name + ' => Password: ' + req.body.password);
+    }
+  }
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), () =>{
+  console.log('El puerto escuchado es el: ', app.get('port'));
 });
